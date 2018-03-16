b5fb259f21dc3714c7a5b0069cebd96e32c065e7
reel16 <te16.elias.renman@umea.nti.se>
2018-03-16 15:05:41 +0100

    test

--- spel.js
+++ spel.js
@@ -22,22 +22,23 @@ var zebraTimer = 0;
 var lionPurchased = 0;
 var buttonHidden = document.getElementById("knappar")
 /* startvÃ¤rden */
-scoreText.textContent = "Points: 0";
-lejonKnapp.textContent = "Lejon: " + lejonCost;
-ZebraButton.textContent = "Zebra: " + zebraCost;
-lionSuper.textContent = "SuperLejon";
+scoreText.textContent = "Pats: 0";
+lejonKnapp.textContent = "More pats: " + lejonCost;
+ZebraButton.textContent = "Patting machine: " + zebraCost;
+lionSuper.textContent = "Pats double up";
+lionSuper.style.display = "none";
 /* click event + logic */
 
 button.addEventListener("click", function() {
 
 	bank += clickValue; // lÃ¤gg till vÃ¤rdet vid click
-	scoreText.textContent = "Points: " + bank; // sÃ¤tt textvÃ¤rdet i p elementet till bank.
+	scoreText.textContent = "Pats: " + bank; // sÃ¤tt textvÃ¤rdet i p elementet till bank.
 }, true);
 shopDropDown.addEventListener("click", function() {
     if (buttonHidden.style.display == "none") {
         buttonHidden.style.display = "block";
     }else{
-        buttonHidden.style.display ="none";
+        buttonHidden.style.display = "none";
     }
 }, true);
 /* kod fÃ¶r zebrapowerup med rÃ¤knare */
@@ -45,20 +46,18 @@ ZebraButton.addEventListener("click", function() {
 	if (bank >= zebraCost) {
 		bank -= zebraCost;
         zebraCost *= 2;
-		powerText.textContent = "Köpte zebra";
-        ZebraButton.textContent = "Zebra: " + zebraCost;
+		powerText.textContent = "You bought a patting machine";
+        ZebraButton.textContent = "Patting machine: " + zebraCost;
 		// LÃ¤gg till setInterval med en funktion som laddas varje sekund
 		zebra = setInterval(function() {
 			bank += 10;
-			scoreText.textContent = "Points: " + bank;
+			scoreText.textContent = "Pats: " + bank;
 			zebraTimer--;
 
 			
 		}, 1000);
-	} else if (zebraTimer > 0) {
-		powerText.textContent = "Du har redan zebra";
 	} else {
-		powerText.textContent = "Du har inte råd med zebra";
+		powerText.textContent = "You can't affoard a patting machine!";
 	}
 }, true);
 lionSuper.addEventListener("click", function() {
@@ -75,11 +74,11 @@ lejonKnapp.addEventListener("click", function() {
         clickValue *= 2;
 		lejonCost *= 2;
 		lejonClicks += 10;
-		lejonKnapp.textContent = "Lejon: " + lejonCost;
-		powerText.textContent = "Köpte lejon";
+		lejonKnapp.textContent = "More pats: " + lejonCost;
+		powerText.textContent = "More pats bought";
 		scoreText.textContent = "Points: " + bank; // sÃ¤tt textvÃ¤rdet i p elementet till bank.
 	} else {
-		powerText.textContent = "Du har inte råd med lejon";
+		powerText.textContent = "You can't affoard more pats";
 	}
     // lägger till möjligheten till superlejon
     if(lionPurchased > 9) {
